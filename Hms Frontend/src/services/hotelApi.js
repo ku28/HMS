b03c10d499f7d7508dc6651497d5b098bfd5e59a
ku28 <ku28sh07@gmail.com>
2026-05-15 12:37:49 +0530

    feat: add hotel API services and update MYSQL_PORT to 3307

--- Hms Frontend/src/services/hotelApi.js
+++ Hms Frontend/src/services/hotelApi.js
@@ -1,31 +1,66 @@
 import api from './api';
 
+const pageResponse = (response, rel) => {
+  const body = response.data || {};
+  const page = body.page || {};
+  return {
+    ...response,
+    data: {
+      data: {
+        content: body._embedded?.[rel] || [],
+        page: page.number || 0,
+        size: page.size || 0,
+        totalElements: page.totalElements || 0,
+        totalPages: page.totalPages || 0,
+      },
+    },
+  };
+};
+
+const itemResponse = (response) => ({
+  ...response,
+  data: {
+    data: response.data,
+  },
+});
+
 export const hotelService = {
-  getAll: (page = 0, size = 12, search = '') =>
-    api.get(`/api/hotels/all?page=${page}&size=${size}&search=${search}`),
-  getById: (id) => api.get(`/api/hotels/${id}`),
-  create: (data) => api.post('/api/hotels/post', data),
-  update: (id, data) => api.put(`/api/hotels/update/${id}`, data),
-  delete: (id) => api.delete(`/api/hotels/delete/${id}`),
+  getAll: (page = 0, size = 12, search = '') => {
+    const params = { page, size };
+    const trimmedSearch = search.trim();
+    if (trimmedSearch) {
+      params.name = trimmedSearch;
+      params.location = trimmedSearch;
+      return api.get('/api/hotels/search/by-name-or-location', { params }).then((res) => pageResponse(res, 'hotels'));
+    }
+    return api.get('/api/hotels', { params }).then((res) => pageResponse(res, 'hotels'));
+  },
+  getById: (id) => api.get(`/api/hotels/${id}`).then(itemResponse),
+  create: (data) => api.post('/api/hotels', data).then(itemResponse),
+  update: (id, data) => api.put(`/api/hotels/${id}`, data).then(itemResponse),
+  delete: (id) => api.delete(`/api/hotels/${id}`),
 };
 
 export const roomService = {
-  getAll: (page = 0, size = 10) => api.get(`/api/room/all?page=${page}&size=${size}`),
-  getById: (id) => api.get(`/api/room/${id}`),
-  getByHotel: (hotelId, page = 0, size = 100) => api.get(`/api/room/hotel/${hotelId}?page=${page}&size=${size}`),
+  getAll: (page = 0, size = 10) =>
+    api.get('/api/rooms', { params: { page, size } }).then((res) => pageResponse(res, 'rooms')),
+  getById: (id) => api.get(`/api/rooms/${id}`).then(itemResponse),
+  getByHotel: (hotelId, page = 0, size = 100) =>
+    api.get('/api/rooms/search/by-hotel', { params: { hotelId, page, size } }).then((res) => pageResponse(res, 'rooms')),
   getAvailableByType: (typeId, page = 0, size = 10) =>
-    api.get(`/api/rooms/available/${typeId}?page=${page}&size=${size}`),
-  create: (data) => api.post('/api/rooms/post', data),
-  update: (id, data) => api.put(`/api/room/update/${id}`, data),
-  delete: (id) => api.delete(`/api/room/delete/${id}`),
+    api.get('/api/rooms/search/available-by-room-type', { params: { roomTypeId: typeId, page, size } }).then((res) => pageResponse(res, 'rooms')),
+  create: (data) => api.post('/api/room-management/rooms', data).then(itemResponse),
+  update: (id, data) => api.put(`/api/room-management/rooms/${id}`, data).then(itemResponse),
+  delete: (id) => api.delete(`/api/room-management/rooms/${id}`),
 };
 
 export const roomTypeService = {
-  getAll: (page = 0, size = 50) => api.get(`/api/RoomType/all?page=${page}&size=${size}`),
-  getById: (id) => api.get(`/api/RoomType/${id}`),
-  create: (data) => api.post('/api/RoomType/post', data),
-  update: (id, data) => api.put(`/api/RoomType/update/${id}`, data),
-  delete: (id) => api.delete(`/api/RoomType/delete/${id}`),
+  getAll: (page = 0, size = 50) =>
+    api.get('/api/room-types', { params: { page, size } }).then((res) => pageResponse(res, 'roomTypes')),
+  getById: (id) => api.get(`/api/room-types/${id}`).then(itemResponse),
+  create: (data) => api.post('/api/room-types', data).then(itemResponse),
+  update: (id, data) => api.put(`/api/room-types/${id}`, data).then(itemResponse),
+  delete: (id) => api.delete(`/api/room-types/${id}`),
 };
 
 export const reservationService = {
@@ -54,12 +89,15 @@ export const reviewService = {
 };
 
 export const amenityService = {
-  getAll: (page = 0, size = 50) => api.get(`/api/amenity/all?page=${page}&size=${size}`),
-  getByHotel: (hotelId) => api.get(`/api/amenity/hotel/${hotelId}`),
-  getByRoom: (roomId) => api.get(`/api/amenity/room/${roomId}`),
-  create: (data) => api.post('/api/amenity/post', data),
-  update: (id, data) => api.put(`/api/amenity/update/${id}`, data),
-  delete: (id) => api.delete(`/api/amenity/${id}`),
+  getAll: (page = 0, size = 50) =>
+    api.get('/api/amenities', { params: { page, size } }).then((res) => pageResponse(res, 'amenities')),
+  getByHotel: (hotelId) =>
+    api.get('/api/amenities/search/by-hotel', { params: { hotelId } }).then((res) => pageResponse(res, 'amenities')),
+  getByRoom: (roomId) =>
+    api.get('/api/amenities/search/by-room', { params: { roomId } }).then((res) => pageResponse(res, 'amenities')),
+  create: (data) => api.post('/api/amenities', data).then(itemResponse),
+  update: (id, data) => api.put(`/api/amenities/${id}`, data).then(itemResponse),
+  delete: (id) => api.delete(`/api/amenities/${id}`),
 };
 
 export const hotelAmenityService = {
